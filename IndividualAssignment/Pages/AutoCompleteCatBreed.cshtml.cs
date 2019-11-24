3a0162945c2d6687d7a6ea659577431c20967848
Aratil <56776378+Aratil@users.noreply.github.com>
2019-11-23 20:44:53 -0500

    added assignment html

---
+++ IndividualAssignment/Pages/AutoCompleteCatBreed.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IndividualAssignment.Pages
+{
+    public class AutoCompleteCatBreedModel : PageModel
+    {
+        [BindProperty]
+        private string Term { get; set; }
+        private IList<string> catBreeds = new List<string>();
+
+        public JsonResult OnGet()
+        {
+            Term = HttpContext.Request.Query["term"];
+
+            AddBreeds("Bengal");
+            AddBreeds("American Shorthair");
+            AddBreeds("British Shorthair");
+            AddBreeds("Persian");
+            AddBreeds("Munchkin");
+            return new JsonResult(catBreeds);
+        }
+        private void AddBreeds(string catBreed)
+        {
+            if (catBreed.Contains(Term))
+            {
+                catBreeds.Add(catBreed);
+            }
+        }
+
+    }
+}
